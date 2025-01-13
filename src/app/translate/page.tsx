66cda12004813b8cd5e3df33d49a837ace8cd442
Rehan Sayyed <125148030+rsayyed591@users.noreply.github.com>
2025-01-13 21:53:52 +0530

    Update page.tsx

--- src/app/translate/page.tsx
+++ src/app/translate/page.tsx
@@ -39,7 +39,7 @@ export default function TranslatePage() {
     tf.ready().then(async () => {
       try {
         const yolov8 = await tf.loadGraphModel(
-          `${window.location.origin}/best_web_model/model.json`,
+          `https://29b6-43-231-238-206.ngrok-free.app/ws`,
           {
             onProgress: (fractions) => {
               setLoading({ loading: true, progress: fractions })
